b2ed8d1dc3e33f62a752ef29011626ca91d1f714
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-20 17:55:07 -0300

    feat: Criado a logica para enviar o codigo para usuarios cadastrados

--- src/services/sendEmail.jsx
+++ src/services/sendEmail.jsx
@@ -1,22 +1,28 @@
 import emailjs from 'emailjs-com';
 import env from 'react-dotenv';
+import validadeUser from './validadeUser';
 
 const generateCoding = Math.floor(Math.random() * env.REACT_APP_NUMBER_VALUE);
-export const sendCode = ({ Subject, message, user }) => (
-  emailjs.send(
-    env.REACT_APP_SERVICE_ID,
-    env.REACT_APP_TEMPLATE_ID,
-    {
-      Subject,
-      message,
-      coding: generateCoding,
-      user,
-    },
-    env.REACT_APP_USER_ID,
-  ).then(() => {
-  }, (error) => {
-    throw new Error(error.text);
-  }));
+export const sendCode = async ({ Subject, message, user }) => {
+  const validade = await validadeUser(user);
+  if (validade) {
+    return emailjs.send(
+      env.REACT_APP_SERVICE_ID,
+      env.REACT_APP_TEMPLATE_ID,
+      {
+        Subject,
+        message,
+        coding: generateCoding,
+        user,
+      },
+      env.REACT_APP_USER_ID,
+    ).then(() => {
+    }, (error) => {
+      throw new Error(error.text);
+    });
+  }
+  return null;
+};
 
 export const sendAccount = ({
   Subject, toName, Email, Senha, user,
--- src/services/validadeUser.jsx
+++ src/services/validadeUser.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable no-console */
 import { getMockAPI } from '../Mocks/Users';
 
-const validadeUser = async (email, password) => {
+const validade = async (value, key) => {
   const data = await getMockAPI();
-  const result = data.some((el) => (
-    el.email === email && el.password === password));
+  if (key === 'email') {
+    const result = data.some((el) => el.email === value);
+    return result;
+  }
+  const result = data.some((el) => el.password === value);
+  return result;
+};
+
+const validadeUser = async (email, password = null) => {
+  if (!password) {
+    const result = await validade(email, 'email');
+    console.log(result);
+    return result;
+  }
+  const result = (await validade(email, 'email') && await validade(password));
   return result;
 };
 
