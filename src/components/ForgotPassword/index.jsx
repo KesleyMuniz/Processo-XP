aad4cdb11abf36c1be7bbfafef450a3d1c30fb48
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-20 11:07:35 -0300

    feat: Iniciando componente de Esqueci minha senha

--- src/components/ForgotPassword/index.jsx
+++ src/components/ForgotPassword/index.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../../context/Context';
 
 export default function ForgotPassword() {
+  const { setRenderForgot, renderForgotPassword } = useContext(Context);
   return (
-    <div>index</div>
+    <div>
+      {renderForgotPassword
+        ? (
+          <div>
+            <h1>
+              Esqueci minha senha
+            </h1>
+
+            <button
+              type="button"
+              onClick={() => { setRenderForgot(!renderForgotPassword); }}
+            >
+              Voltar
+            </button>
+
+          </div>
+        ) : (null)}
+    </div>
   );
 }
