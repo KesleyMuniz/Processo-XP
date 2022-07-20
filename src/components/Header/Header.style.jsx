e1087ce049a4ddeff27f530a1035d2b12619a5d6
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-19 23:35:31 -0300

    style: Criado Styled Components para o Header

---
+++ src/components/Header/Header.style.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  color: white;
+  display: flex;
+  width: 100%;
+  height: 180px;
+  background-image: linear-gradient(to right, #45b8e6 , #d66a9f);
+  border-radius: 10px 5px 12em / 0 0 100%;
+  justify-content: flex-end;
+  @media(max-width: 800px) {
+    justify-content: flex-start;
+}
+`;
+
+export const DivName = styled.div`
+  display: flex;
+  position: absolute;
+  font-family: 'Roboto', sans-serif;
+  font-size: 200px;
+  font-weight: 900;
+  margin-top: -10px;
+  margin-right: 400px;
+  @media(max-width: 800px) {
+}
+`;
+
+export const DivLastName = styled.div`
+  position: absolute;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 200;
+  margin-top: 141px;
+  margin-right: 200px;
+  font-size: 40px;
+  @media(max-width: 800px) {
+    padding-left: 200px;
+}
+`;
