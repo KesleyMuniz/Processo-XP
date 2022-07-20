a3a713effa72a691aaa6abc50eec2f3e9f57fcb4
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-19 23:34:19 -0300

    feat: createGlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import * as Page from './pages';
+import GlobalStyle from './assets/css/styleGlobal';
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Page.Login />} />
         <Route path="/Registration" element={<Page.Registration />} />
