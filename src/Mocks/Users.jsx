de37c7a65e31d34d264b90fb8e889587bd19f904
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-20 03:23:58 -0300

    feat: Mock para testar logica de verificar o usuario

---
+++ src/Mocks/Users.jsx
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    email: 'test@test.com',
+    password: 'Test_test123',
+  },
+  {
+    id: 1,
+    email: 'test2@test.com',
+    password: 'Test_test123',
+  },
+  {
+    id: 1,
+    email: 'test3@test.com',
+    password: 'Test_test123',
+  },
+  {
+    id: 1,
+    email: 'test6@test.com',
+    password: 'Test_test123',
+  },
+  {
+    id: 1,
+    email: 'test5@test.com',
+    password: 'Test_test123',
+  },
+];
