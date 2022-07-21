0062892fe6088794ad5832d6c548bc1b598c1d4f
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-21 08:20:08 -0300

    feat: Criado o componente Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import * as S from './sidebar.style';
+import * as Icons from '../../assets/icons';
+import Context from '../../context/Context';
+import { replaceNames } from '../../services';
 
 export default function Sidebar() {
+  const { userData } = useContext(Context);
   return (
-    <div>index</div>
+    <S.SpanSidebarContainer>
+      <S.ULContainer>
+        <S.LI>
+          <S.DivUser>
+            {replaceNames(userData.name)}
+            <img src={Icons.User} alt="Logo do usuário" />
+          </S.DivUser>
+        </S.LI>
+        <S.LI>
+          <S.DivUser>
+            {`${userData.AccountBalance} R$`}
+            <img src={Icons.Balance} alt="Logo do usuário" />
+          </S.DivUser>
+        </S.LI>
+      </S.ULContainer>
+    </S.SpanSidebarContainer>
   );
 }
