c30d8ca7e97fbfaa6e8ff6f7799259e0c9fad4db
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-22 12:16:01 -0300

    style: Estilo do botao da janela

--- src/components/Negotiate/Negotiate.style.jsx
+++ src/components/Negotiate/Negotiate.style.jsx
@@ -2,13 +2,29 @@ import styled from 'styled-components';
 
 export const BG = styled.div`
   background-image: linear-gradient(to right, #296e8a , #723854);
-  display: flex;
   height: 80%;
-  justify-content: center;
   position: fixed;
-  width: 80%;
+  width: 60%;
   border-radius: 5%;
   z-index: 999;
+`;
+export const Header = styled.header`
+  display: flex;
+  width: auto;
+  justify-content: flex-end;
+  button {
+    background-color: #D16CA1;
+    margin-right: 50px;
+    margin-top: -18px;
+    width: 77px;
+    height: 37px;
+    border-radius: 15px;
+    border-style: none;
+    font-size: 25px;
+   :hover{
+      background-color: white;
+    }
+  }
 `;
 export const Container = styled.div`
   width: 100%;
