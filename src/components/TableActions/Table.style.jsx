d69b756c041ed83b8fb9e0af90ed0e213913d5ea
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-22 09:47:09 -0300

    style: Aplicado um css basico para melhor visualização da tabela

---
+++ src/components/TableActions/Table.style.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+display: flex;
+align-items: center;
+flex-direction: column;
+font-size: 30px;
+`;
+
+export const Thead = styled.thead`
+
+`;
+
+export const Tr = styled.tr`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+export const Th = styled.th`
+  margin: auto;
+`;
+
+export const Tbody = styled.tbody`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Td = styled.td`
+  display: flex;
+  margin: auto;
+`;
+
+export const Button = styled.button`
+  width: 70px;
+  height: 20px;
+`;
