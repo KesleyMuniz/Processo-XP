a487fb0dc042aefc4abe6cf8d43867a01537ee7a
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-22 10:39:41 -0300

    feat: Criada a logica para verificar se ja existe uma requisição da API salva

--- src/components/AvailableActions/index.jsx
+++ src/components/AvailableActions/index.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { getAPI } from '../../services';
+import { getAPI, getSessionStorage } from '../../services';
 import TableActions from '../TableActions';
 
 export default function AvailableActions() {
   const [actionsStatus, setActions] = useState(null);
   const [authorizationRender, setAuthorization] = useState(false);
+  const actionsSession = getSessionStorage('actions');
 
   function merged(values, names) {
     const results = names.map((screen) => ({
       ...values.find((o) => o.T === screen.cd_acao_rdz),
       name: screen.nm_empresa_rdz,
     }));
+
     if (results) setActions(results);
   }
   const filterActionsAndNames = (apiNames, apiValues) => {
@@ -24,13 +26,17 @@ export default function AvailableActions() {
   };
 
   const getActionsData = async () => {
-    const date = new Date();
-    const formatDate = `${date.getFullYear()}-0${date.getMonth() + 1}-${+date.getDate() - 2}`;
-    const URL_ACTIONS_REAL_TIME = `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/${formatDate}?adjusted=true&apiKey=336BWBp6mP2tmzuKjyIHeMWy_FIfOJWc`;
-    const URL_NAMES_API = 'https://api-cotacao-b3.labdo.it/api/cotacao/dt/20200102/02';
-    const actions = await getAPI(URL_ACTIONS_REAL_TIME);
-    const names = await getAPI(URL_NAMES_API);
-    filterActionsAndNames(names, actions.results);
+    if (actionsSession) {
+      setActions(actionsSession);
+    } else {
+      const date = new Date();
+      const formatDate = `${date.getFullYear()}-0${date.getMonth() + 1}-${+date.getDate() - 2}`;
+      const URL_ACTIONS_REAL_TIME = `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/${formatDate}?adjusted=true&apiKey=336BWBp6mP2tmzuKjyIHeMWy_FIfOJWc`;
+      const URL_NAMES_API = 'https://api-cotacao-b3.labdo.it/api/cotacao/dt/20200102/02';
+      const actions = await getAPI(URL_ACTIONS_REAL_TIME);
+      const names = await getAPI(URL_NAMES_API);
+      filterActionsAndNames(names, actions.results);
+    }
   };
 
   useEffect(() => {
