a314068b44d0bbd83b958e175b208088650ed4d9
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-24 09:12:07 -0300

    feat: Renderização condicional para a tabela caso nao tenha ações

--- src/components/TableMyActions/index.jsx
+++ src/components/TableMyActions/index.jsx
@@ -7,45 +7,49 @@ export default function TableMyActions({ itens }) {
   const randomID = () => useId();
   const { setOpen, setSelected } = useContext(Context);
   return (
-    <S.Table>
-      <S.Thead>
-        <S.Tr>
-          <S.Th>Ação</S.Th>
-          <S.Th>Empresa</S.Th>
-          <S.Th>Volume</S.Th>
-          <S.Th>Abertura</S.Th>
-          <S.Th>Baixo</S.Th>
-          <S.Th>Médio</S.Th>
-          <S.Th>Custo total</S.Th>
-          <S.Th>Negociar</S.Th>
-        </S.Tr>
-      </S.Thead>
-      <S.Tbody>
-        {itens.map((item) => (
-          <S.Tr key={`${randomID()}`}>
-            <S.Td>{item.T}</S.Td>
-            <S.Td>{item.name}</S.Td>
-            <S.Td>{(+item.v).toFixed(0)}</S.Td>
-            <S.Td>{(+item.o).toFixed(2)}</S.Td>
-            <S.Td>{(+item.l).toFixed(2)}</S.Td>
-            <S.Td>{(+item.amount).toFixed(2)}</S.Td>
-            <S.Td>{(+item.h).toFixed(2)}</S.Td>
-            <S.Td>
-              <button
-                value={item.T}
-                type="button"
-                onClick={() => {
-                  setOpen(true);
-                  setSelected(item);
-                }}
-              >
-                Negociar
-              </button>
-            </S.Td>
-          </S.Tr>
-        ))}
-      </S.Tbody>
-    </S.Table>
+    <div>
+      {itens && (
+        <S.Table>
+          <S.Thead>
+            <S.Tr>
+              <S.Th>Ação</S.Th>
+              <S.Th>Empresa</S.Th>
+              <S.Th>Volume</S.Th>
+              <S.Th>Abertura</S.Th>
+              <S.Th>Baixo</S.Th>
+              <S.Th>Médio</S.Th>
+              <S.Th>Custo total</S.Th>
+              <S.Th>Negociar</S.Th>
+            </S.Tr>
+          </S.Thead>
+          <S.Tbody>
+            {itens.map((item) => (
+              <S.Tr key={`${randomID()}`}>
+                <S.Td>{item.T}</S.Td>
+                <S.Td>{item.name}</S.Td>
+                <S.Td>{(+item.v).toFixed(0)}</S.Td>
+                <S.Td>{(+item.o).toFixed(2)}</S.Td>
+                <S.Td>{(+item.l).toFixed(2)}</S.Td>
+                <S.Td>{(+item.amount).toFixed(2)}</S.Td>
+                <S.Td>{(+item.h).toFixed(2)}</S.Td>
+                <S.Td>
+                  <button
+                    value={item.T}
+                    type="button"
+                    onClick={() => {
+                      setOpen(true);
+                      setSelected(item);
+                    }}
+                  >
+                    Negociar
+                  </button>
+                </S.Td>
+              </S.Tr>
+            ))}
+          </S.Tbody>
+        </S.Table>
+      )}
+    </div>
   );
 }
 
