2e86d1108d7095303ebcad036fac7e2a4f4a6b63
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-24 16:26:28 -0300

    feat: Pagina profile Criada

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Comp from '../../components';
+
+export default function Profile() {
+  return (
+    <>
+      <Comp.Header Logged userData />
+      <div>Pagina em contração</div>
+      <Comp.Footer />
+    </>
+  );
+}
