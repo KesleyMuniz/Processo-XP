69a45162c8a63909320869a964ec6759ac9cb8c3
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-24 20:46:04 -0300

    feat: Criada a logica de deslogar o usuario

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as Comp from '../../components';
 
 export default function Profile() {
+  const navigate = useNavigate();
   return (
-    <>
+    <div>
       <Comp.Header Logged userData />
       <div>Pagina em contração</div>
+
+      <button
+        type="button"
+        onClick={() => {
+          navigate('/');
+          localStorage.removeItem('login');
+        }}
+      >
+        Sair
+
+      </button>
       <Comp.Footer />
-    </>
+    </div>
   );
 }
