4ae6ed2ead14050e6a531336ae1dc3bb74ceefb8
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-24 05:08:15 -0300

    feat: Aplicando os componentes

--- src/components/Negotiate/index.jsx
+++ src/components/Negotiate/index.jsx
@@ -4,11 +4,10 @@ import Context from '../../context/Context';
 import { getSessionStorage } from '../../services';
 import ButtonBuy from '../ButtonBuy';
 import UserBalance from '../UserBalance';
+import * as Com from '../commons';
 
 export default function Negotiate() {
-  const {
-    setNegotiation, selectedAction, setPurchase, statusDisable, setSendBuy,
-  } = useContext(Context);
+  const { selectedAction, setPurchase, statusDisable } = useContext(Context);
 
   const [buyVolumes, setBuy] = useState(0);
   const [Stock, setStock] = useState(null);
@@ -84,124 +83,66 @@ export default function Negotiate() {
     <S.Container>
       <S.BG>
         <S.Header>
-          <button
-            type="button"
-            onClick={() => {
-              setNegotiation(false);
-              setSendBuy(false);
-            }}
-          >
-            X
-          </button>
+          <Com.ButtonClose />
         </S.Header>
         <div>
           {Stock ? (
             <>
               <div>
                 <UserBalance />
-                <div>
-                  <span>Valor médio por volume</span>
-                  <div>{(+Stock[0].vw).toFixed(2).replace('.', ',')}</div>
-                </div>
+                <Com.AverageValue value={+Stock[0].vw} />
               </div>
-              <div>
-                Opções de compras
-              </div>
-              <div>
-                <label htmlFor="valueInputValue">
-                  <input
-                    type="radio"
-                    id="valueInputValue"
-                    name="optionsMethod"
-                    checked={calculatedOptions}
-                    readOnly
-                    onClick={() => setOptions(true)}
-                  />
-                  valor
-                </label>
-              </div>
-              <label htmlFor="valueInputVolume">
-                <input
-                  type="radio"
-                  id="valueInputVolume"
-                  name="optionsMethod"
-                  checked={!calculatedOptions}
-                  readOnly
-                  onClick={() => setOptions(false)}
-                />
-                volume
-              </label>
-              <div>
-                <input
-                  type="number"
-                  id="buyInput"
-                  placeholder="Numeros e virgula"
-                  value={buyVolumes}
-                  onChange={(e) => { setBuy(e.target.value); }}
-                />
-              </div>
+              <Com.PurchaseOptions calculatedOptions={calculatedOptions} setOptions={setOptions} />
+              <Com.InputValue
+                buyVolumes={buyVolumes}
+                setBuy={setBuy}
+              />
               <div>
                 {calculatedOptions ? (
                   <>
-                    <div>
-                      <span>Volume disponível</span>
-                      <div>{(Stock[0].v) - Math.floor(calculatedValue)}</div>
-                    </div>
-                    <span>Volume Final</span>
-                    <div>{Math.floor(calculatedValue)}</div>
-                    <div>
-                      <button
-                        type="button"
-                        value={+Stock[0].vw}
-                        disabled={statusDisable}
-                        onClick={(e) => {
-                          calculateButton(e.target.value, 'sum');
-                        }}
-                      >
-                        +
-
-                      </button>
-                    </div>
-                    <div>
-                      <button
-                        type="button"
-                        value={+Stock[0].vw}
-                        onClick={(e) => {
-                          calculateButton(e.target.value, 'subtraction');
-                        }}
-                      >
-                        -
-                      </button>
-                    </div>
+                    <Com.AvailableVolume
+                      value={Stock[0].v}
+                      calculatedValue={calculatedValue}
+                      calculatedOptions
+                    />
+                    <Com.FinalVolume value={calculatedValue} />
+                    <Com.PlusAndMinusButtons
+                      value={+Stock[0].vw}
+                      disabled={statusDisable}
+                      calculateButton={calculateButton}
+                      type="sum"
+                      name="+"
+                    />
+                    <Com.PlusAndMinusButtons
+                      value={+Stock[0].vw}
+                      disabled={false}
+                      calculateButton={calculateButton}
+                      type="subtraction"
+                      name="-"
+                    />
                   </>
 
                 ) : (
                   <>
-                    <div>
-                      <span>Volume disponível</span>
-                      <div>{(Stock[0].v) - buyVolumes}</div>
-                    </div>
-                    <span>Valor total</span>
-                    <div>{calculatedValue.toFixed(2).replace('.', ',')}</div>
-                    <div>
-                      <button
-                        type="button"
-                        disabled={statusDisable}
-                        onClick={() => { setBuy(buyVolumes + 1); }}
-                      >
-                        +
-                      </button>
-                    </div>
-                    <div>
-                      <button
-                        type="button"
-                        onClick={() => {
-                          calculateButton(1, 'subtraction');
-                        }}
-                      >
-                        -
-                      </button>
-                    </div>
+                    <Com.AvailableVolume
+                      value={Stock[0].v}
+                      calculatedValue={buyVolumes}
+                      calculatedOptions={false}
+                    />
+                    <Com.Amount value={calculatedValue} />
+                    <Com.NegativeAndPositiveButtons
+                      disabled={statusDisable}
+                      subtraction={false}
+                      functionOption={setBuy}
+                      buyVolumes={buyVolumes}
+                      name="+"
+                    />
+                    <Com.NegativeAndPositiveButtons
+                      disabled={false}
+                      subtraction
+                      functionOption={calculateButton}
+                      name="-"
+                    />
                   </>
                 )}
               </div>
