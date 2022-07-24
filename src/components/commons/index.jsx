517b5444ade09fcff96f92d40efd0a7e7316f151
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-07-24 05:07:53 -0300

    feat: Centralizando o export dos components

---
+++ src/components/commons/index.jsx
@@ -0,0 +1,21 @@
+import AverageValue from './CompNegotiate/AverageValue';
+import ButtonClose from './CompNegotiate/ButtonClose';
+import PurchaseOptions from './CompNegotiate/PurchaseOptions';
+import AvailableVolume from './CompNegotiate/AvailableVolume';
+import PlusAndMinusButtons from './CompNegotiate/PlusAndMinusButtons';
+import Amount from './CompNegotiate/Amount';
+import NegativeAndPositiveButtons from './CompNegotiate/NegativeAndPositiveButtons';
+import InputValue from './CompNegotiate/InputValue';
+import FinalVolume from './CompNegotiate/FinalVolume';
+
+export {
+  AverageValue,
+  ButtonClose,
+  PurchaseOptions,
+  AvailableVolume,
+  PlusAndMinusButtons,
+  Amount,
+  NegativeAndPositiveButtons,
+  InputValue,
+  FinalVolume,
+};
